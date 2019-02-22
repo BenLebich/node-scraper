e03ed59bc8f835ad5c6de49d50f73acd87ae5049
Ben Lebich <benlebich@gmail.com>
2019-02-22 14:14:27 -0800

    setting up working get request

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 var http = require('http');
+var request = require('request');
 
 //create a server object:
 http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
-  res.end(); //end the response
-}).listen(process.env.PORT || 80); //the server object listens on port 8080
+  res.write('Hello World!');
+  request('http://www.benarea.com/', function (error, response, body) {
+    //console.log(response);
+    if (!error && response.statusCode == 200) {
+      //var info = JSON.parse(body)
+      //console.log(body);
+      res.write(body);
+      res.end();
+    }
+  });
+  
+}).listen(process.env.PORT || 80);
